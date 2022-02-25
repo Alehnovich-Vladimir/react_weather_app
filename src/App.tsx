c12d2bf94352659a93ddf0dccb341d638c20c179
AlexVovan <alehnovich.vova@gmail.com>
2022-02-25 17:29:37 +0100

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { Home } from './pages/Home/Home';
+import { MonthStatistics } from './pages/MonthStatistics/MonthStatistics';
+import { Header } from './shared/Header/Header';
 
 function App() {
   return (
-    <div className="App">Hello</div>
-  );
+    <div className="container">
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/month-statistics" exact component={MonthStatistics} />
+      </Switch>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/pages/Home/components/ThisDay/ThisDay.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './ThisDay.module.scss';
+
+interface Props {}
+
+export const ThisDay = (props: Props) => {
+    return <header className={s.this__day}>
+      <div></div>
+    </header>;
+}
