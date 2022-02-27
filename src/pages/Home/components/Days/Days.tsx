146bdb07b8ee125a283b4e5a9f939102e1eb01c7
AlexVovan <alehnovich.vova@gmail.com>
2022-02-27 15:08:35 +0100

    add tabs

--- src/pages/Home/components/Days/Days.tsx
+++ src/pages/Home/components/Days/Days.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card } from './Card';
 import s from './Days.module.scss';
+import { Tabs } from './Tabs';
 
 interface Props {}
 
@@ -72,11 +73,14 @@ export const Days = (props: Props) => {
           info: 'Облачно',
         },
       ];
-    return (
+  return (
+    <>
+      <Tabs />
         <div className={s.days}>
             {days.map((day: Day) => (
                 <Card day={day} />
             ))}
         </div>
+      </>
     );
 };
